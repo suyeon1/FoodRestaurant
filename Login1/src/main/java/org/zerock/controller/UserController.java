68ac3504df4918261cb45b5120636dce12c3bbf2
0kang630 <0kang630@naver.com>
2020-08-04 17:14:20 +0900

    lg 01

---
+++ Login1/src/main/java/org/zerock/controller/UserController.java
@@ -0,0 +1,15 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("user/*")
+public class UserController {
+
+	
+
+	
+}
---
+++ Login1/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<jsp:forward page="/user/login"/>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SpringEx01 커뮤니티</title>
+</head>
+<body>
+</body>
+</html>
